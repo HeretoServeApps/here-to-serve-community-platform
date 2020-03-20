97a9ab4d3960d22938104321f3413195bbf33684
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-03-20 15:35:16 -0400

    Removed venv2

--- venv2/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/dzungnguyen/OneDrive - PennO365/h4i/here-to-serve/venv2/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
