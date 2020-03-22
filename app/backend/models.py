29e372cfea21c8d2f751f8eaea09c3a162311db5
Lisa Moshiro <lmmoshiro2000@gmail.com>
2020-03-22 15:58:29 -0400

    added new fields

--- app/backend/models.py
+++ app/backend/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 class Community(models.Model):
     name = models.CharField(max_length=128, blank=False)
+    purpose = models.CharField(max_length=128, blank=False)
     is_closed = models.BooleanField(blank=False)
+    description = models.CharField(max_lenngth=128, blank=False)
+    zipcode = models.IntegerField(blank=False)
+    country = models.CharField(max_length=128, blank=False)
 
     def __str__(self):
         return self.name
