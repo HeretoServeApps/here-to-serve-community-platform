d02de94686308e1e58bcbd0b57346552e8478fcf
Lisa Moshiro <lmmoshiro2000@gmail.com>
2020-03-24 18:04:00 -0400

    edited fields by adding defaults

--- app/backend/models.py
+++ app/backend/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Community(models.Model):
-    name = models.CharField(max_length=128, blank=False)
-    purpose = models.CharField(max_length=128, blank=False)
-    is_closed = models.BooleanField(blank=False)
-    description = models.CharField(max_lenngth=128, blank=False)
-    zipcode = models.IntegerField(blank=False)
-    country = models.CharField(max_length=128, blank=False)
+    # defaults were just for already existing rows, no real meaning
+    name = models.CharField(max_length=128, blank=False, default='')
+    purpose = models.CharField(max_length=128, blank=False, default='')
+    is_closed = models.BooleanField(blank=False, default=False)
+    description = models.CharField(max_length=256, blank=False, default='')
+    zipcode = models.IntegerField(blank=False, default=0)
+    country = models.CharField(max_length=128, blank=False, default='')
+    coordinator = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
