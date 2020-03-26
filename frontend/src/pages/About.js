57dc6e9355c6ba56d5dfee49986e19ba5c9fd3af
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-03-26 12:56:19 -0400

    Added login page (frontend only)

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import { Text } from "evergreen-ui"
+
+export default function About() {
+    return (
+        <Text>About Us</Text>
+    );
+}
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import { Text } from "evergreen-ui"
+
+export default function Home() {
+    return (
+        // Insert code from Grace
+        <Text>Home</Text>
+    );
+}
