f464e3f5990deba5c9940584eab5f7959e0dcd93
Amit Lohe <amit.lohe101@gmail.com>
2020-03-29 14:50:02 -0700

    Remove role field from UserManager

--- app/backend/models.py
+++ app/backend/models.py
@@ -39,8 +39,7 @@ class Community(models.Model):
 
 class UserManager(BaseUserManager):
     def create_user(
-        self, email, first_name, last_name, phone_number, role, password=None, 
-        commit=True):
+        self, email, first_name, last_name, phone_number, password=None, commit=True):
 
         if not first_name:
             raise ValueError(_('Users must have a first name'))
@@ -50,15 +49,12 @@ class UserManager(BaseUserManager):
             raise ValueError(_('Users must have a phone number'))
         if not email:
             raise ValueError(_('Users must have an email address'))
-        if not role:
-            raise ValueError(_('Users must have a role'))
 
         user = self.model(
                 first_name=first_name,
                 last_name=last_name,
                 phone_number=phone_number,
-                email=self.normalize_email(email),
-                role=role
+                email=self.normalize_email(email)
             )
 
         user.set_password(password)
@@ -66,15 +62,13 @@ class UserManager(BaseUserManager):
             user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, first_name, last_name, phone_number, role, password=None, 
-        commit=True):
+    def create_superuser(self, email, first_name, last_name, phone_number, password=None, commit=True):
         user = self.create_user(
             email=self.normalize_email(email),
             password=password,
             first_name=first_name,
             last_name=last_name,
             phone_number=phone_number,
-            role=role,
             commit=False,
         )
         user.is_staff = True
@@ -94,7 +88,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number','role']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number']
 
     def get_full_name(self):
         full_name = '%s %s' % (self.first_name, self.last_name)
@@ -102,6 +96,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return '{} <{}>'.format(self.get_full_name(), self.email)
+
 
 class CommunityUserRole(models.Model):
     ADMIN = 'ADMIN'
