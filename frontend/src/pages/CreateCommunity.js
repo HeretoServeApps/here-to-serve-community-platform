efff80f3107cdc1e7b4d00866d5e8d9b604b20e6
cx-lu <cxlu@wharton.upenn.edu>
2020-03-31 18:53:23 -0400

    Revised form

--- frontend/src/pages/CreateCommunity.js
+++ frontend/src/pages/CreateCommunity.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import countryList from 'react-select-country-list'
 
 import {
   Field,
   Checkbox,
   Control,
+  Input,
   Select,
-  Label
+  Textarea
 } from 'react-bulma-components/lib/components/form'
 import Button from 'react-bulma-components/lib/components/button'
 import Container from 'react-bulma-components/lib/components/container'
@@ -24,7 +26,7 @@ export default function CreateCommunity() {
   // Non-bulma styles
   var containerStyle = {
     margin: '10% auto',
-    maxWidth: '550px',
+    maxWidth: '700px',
     padding: '4rem',
     border: '0.1rem solid #E5E5E5',
     borderRadius: '1rem'
@@ -34,43 +36,54 @@ export default function CreateCommunity() {
     fontSize: '0.75rem',
     fontStyle: 'italic'
   }
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [zipCode, setZipCode] = useState('')
+  const [country, setCountry] = useState('United States')
   return (
     <Container style={containerStyle}>
       <Heading size={4}>Create New Community</Heading>
       <Heading size={6}>Basic Information</Heading>
       <Field>
         <Control>
-          <input
-            name='name'
-            class='input'
-            type='text'
+          <Input
+            value={name}
+            onChange={e => setName(e.target.value)}
             placeholder='Community Name'
           />
         </Control>
       </Field>
       <Field>
-        <input
-          name='purpose'
-          class='input'
-          type='text'
-          placeholder='Community Purpose'
+        <Textarea
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+          placeholder='Community Description'
         />
       </Field>
       <Field>
         <Columns>
           <Columns.Column>
             <Field>
-              <input
-                name='zip-code'
-                class='input'
-                type='text'
+              <Input
+                value={zipCode}
+                onChange={e => setZipCode(e.target.value)}
                 placeholder='Zip Code'
               />
             </Field>
           </Columns.Column>
           <Columns.Column>
-            <Select name='country'>
-              <option value=''>Country</option>
+            <Select
+              name='country'
+              value={country}
+              onChange={e => setCountry(e.target.value)}
+            >
+              {countryList()
+                .getLabels()
+                .map(c => (
+                  <option style={{ position: 'static' }} value={c}>
+                    {c}
+                  </option>
+                ))}
             </Select>
           </Columns.Column>
         </Columns>
