b749e189844826acbfbe3be59568362d9d690e56
Lisa Moshiro <lmmoshiro2000@gmail.com>
2020-04-01 16:42:16 -0400

    added most fields

---
+++ app/backend/management/commands/fake-communities.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand, CommandError
+from backend.models import Community
+from faker import Faker
+from random import choice
+import random
+class Command(BaseCommand):
+    help = 'Creates n fake communities'
+
+    def add_arguments(self, parser):
+        parser.add_argument('num_communities', type=int)
+
+    def handle(self, *args, **options):
+        fake = Faker()
+        num_users = options['num_users']
+        community = Community.query.all()
+
+        for i in range(num_users):
+            fake_name = fake.company()
+            fake_purpose = fake.sentence()
+            fake_is_closed = random.choice(["Yes", "No"])
+            fake_description = fake.sentence()
+            fake_country = fake.country()
+            fake_zipcode = fake.zipcode()
+            fake_coordinator = ""
+
+            c = Community(
+                name=fake_name,
+                purpose=fake_purpose,
+                is_closed=fake_is_closed,
+                description=fake_description,
+                country=fake_country,
+                zipcode=fake_zipcode,
+                password='password',
+                confirmed=True,
+                coordinator=fake_coordinator)
+            c.save()
+        self.stdout.write(self.style.SUCCESS('Successfully created fake communities'))
+
