84a7d04d22a3c3b5a8502abb0fe7ba5611cea787
Lisa Moshiro <lmmoshiro2000@gmail.com>
2020-03-31 21:13:40 -0400

    small edits

--- app/backend/management/commands/fakedatacommand.py
+++ app/backend/management/commands/fakedatacommand.py
@@ -1,9 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
 from backend.models import Community
+from faker import Faker
 class Command(BaseCommand):
     help = 'Creates fake data'
 
     def handle(self, *args, **options):
-
-         self.stdout.write(self.style.SUCCESS('Successfully created fake data'))
+        fake = Faker()
+        community = Community.query.all()
+        self.stdout.write(self.style.SUCCESS('Successfully created fake data'))
 
