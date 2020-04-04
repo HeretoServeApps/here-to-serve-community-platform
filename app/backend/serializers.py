e73cac60210a90678673e3f22b08a36963e19153
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-04-03 21:02:44 -0400

    fixed typo

--- app/backend/serializers.py
+++ app/backend/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-]from rest_framework_jwt.settings import api_settings
+from rest_framework_jwt.settings import api_settings
 from .models import Community, User
 
 
