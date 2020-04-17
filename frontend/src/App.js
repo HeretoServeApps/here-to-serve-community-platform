f1376cc0b708ba42d2a696c71ecad6f88658e92b
cx-lu <cxlu@wharton.upenn.edu>
2020-04-16 20:03:15 -0400

    Added forgot password page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import { 
-  Route, 
+import {
+  Route,
   Switch,
   BrowserRouter as Router,
   useHistory,
@@ -14,50 +14,63 @@ import CreateCommunity from './pages/CreateCommunity'
 import Login from './pages/Login'
 import About from './pages/About'
 import Welcome from './pages/Welcome'
+import ForgotPassword from './pages/ForgotPassword'
 
 export default function App() {
-  const [loggedIn, setLoggedIn] = useState(localStorage.getItem('token') ? true : false)
+  const [loggedIn, setLoggedIn] = useState(
+    localStorage.getItem('token') &&
+      localStorage.getItem('token') !== 'undefined'
+      ? true
+      : false
+  )
 
   const handleLogin = useCallback((email, password) => {
     fetch('/token-auth/', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        'email': email,
-        'password': password
-      })
+        email: email,
+        password: password,
+      }),
     })
-      .then(res => res.json())
-      .then(json => {
-        localStorage.setItem('token', json.token)
-        JSON.stringify(json.token) ? setLoggedIn(true) : setLoggedIn(false)
-      },
+      .then((res) => res.json())
+      .then(
+        (json) => {
+          localStorage.setItem('token', json.token)
+          JSON.stringify(json.token) ? setLoggedIn(true) : setLoggedIn(false)
+        },
         (error) => {
-          console.log(error);
-        })
+          console.log(error)
+        }
+      )
   }, [])
 
   return (
     <div>
       <Router>
-        <Header 
-          logged_in = {loggedIn}
-        />
+        <Header logged_in={loggedIn} />
         <Switch>
-            <Route path="/my-communities" exact component={MyCommunities}/>
-            <Route path="/register" exact component={Register}/>
-            <Route path="/select-communities" exact component={SelectCommunities}/>
-            <Route path='/create-community' exact component={CreateCommunity} />
-            <Route path='/login' render={
-              () => <Login handle_login={handleLogin} logged_in={loggedIn}/>}
-            />
-            <Route path='/about' exact component={About} />
-            <Route path='/' exact component={Welcome} />
+          <Route path='/my-communities' exact component={MyCommunities} />
+          <Route path='/register' exact component={Register} />
+          <Route
+            path='/select-communities'
+            exact
+            component={SelectCommunities}
+          />
+          <Route path='/create-community' exact component={CreateCommunity} />
+          <Route
+            path='/login'
+            render={() => (
+              <Login handle_login={handleLogin} logged_in={loggedIn} />
+            )}
+          />
+          <Route path='/about' exact component={About} />
+          <Route path='/' exact component={Welcome} />
+          <Route path='/forgot-password' exact component={ForgotPassword} />
         </Switch>
       </Router>
     </div>
-  );
+  )
 }
-
