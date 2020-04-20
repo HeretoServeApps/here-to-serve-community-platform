40863e0578c1d251bcde762551b348febde447d7
Ivan <ivanes@seas.upenn.edu>
2020-04-20 02:40:24 -0400

    added email settings page

--- frontend/src/pages/EmailSettings.js
+++ frontend/src/pages/EmailSettings.js
@@ -5,6 +5,20 @@ import Columns from 'react-bulma-components/lib/components/columns'
 import Heading from 'react-bulma-components/lib/components/heading'
 
 import SideNavAccount from '../components/sideNavAccount'
+import ToggleSwitch from '../components/switch'
+
+import { Link } from 'react-router-dom'
+import Button from 'react-bulma-components/lib/components/button'
+import Media from 'react-bulma-components/lib/components/media'
+import Content from 'react-bulma-components/lib/components/content'
+import Card from 'react-bulma-components/lib/components/card'
+import styled from 'styled-components'
+
+
+const ButtonBg = styled.div`
+  background-color: white;
+  border-radius: 5px;
+`
 
 export default function AccountSettings() {
     var containerStyle = {
@@ -20,10 +34,55 @@ export default function AccountSettings() {
                 <Columns.Column style={{ maxWidth:'30%' }}>
                     <SideNavAccount />
                 </Columns.Column>
+
                 <Columns.Column>
-                    Ivan is making this thanks Ivan.
+                <div>
+                   <ToggleSwitch/> <a style={{ marginLeft: '1.5rem' }}> </a>
+                   Information about Service Upgrades 
+                </div>
+                <br/>
+                <div>
+                   <ToggleSwitch/> <a style={{ marginLeft: '1.5rem' }}> </a>
+                   Lotsa Community Works Newsletter
+                </div>
+                 <br/>
+                <div>
+                   <ToggleSwitch/> <a style={{ marginLeft: '1.5rem' }}> </a>
+                   Information about Here To Serve
+                </div>
+                <br/>
+        
+                <Link to='/'>
+                <Button
+                  style={{
+                    marginBottom: '1rem',
+                    boxShadow: '1px 1px 3px 2px rgba(0,0,0,0.1)',
+                    maxWidth:250,
+
+                  }}
+                  color='primary'
+                  fullwidth={true}
+                >
+                  SAVE CHANGES
+                </Button>
+              </Link>
+                <Link to='/'>
+                  <Button
+                    style={{
+                      boxShadow: '1px 1px 3px 2px rgba(0,0,0,0.1)',
+                    maxWidth:250,
+                    }}
+                    fullwidth={true}
+                    outlined={true}
+                    color='primary'
+                  >
+                    CANCEL
+                  </Button>
+                </Link>
                 </Columns.Column>
             </Columns>
+
+          
         </Container>
     );
 }
