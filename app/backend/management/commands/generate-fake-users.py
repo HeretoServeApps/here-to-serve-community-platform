3c63fd46031a2fd941d2e3f538b8ae0ae7414877
Lisa Moshiro <lmmoshiro2000@gmail.com>
2020-04-24 14:20:26 -0400

    added new fields

--- app/backend/management/commands/generate-fake-users.py
+++ app/backend/management/commands/generate-fake-users.py
@@ -15,11 +15,24 @@ class Command(BaseCommand):
 
         for i in range(1, num_users+1):
             User.objects.create_user(
-                fake.email(),
-                fake.first_name(),
-                fake.last_name(),
-                fake.phone_number(),
-                'password', 
+                email=fake.email(),
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                address_line_1=fake.street_address(),
+                address_line_2='apt 1',
+                city=fake.city(),
+                state=fake.state(),
+                zipcode=fake.postcode(),
+                country='US',
+                phone_number_1=fake.phone_number(),
+                phone_number_1_type='cell',
+                phone_number_2=fake.phone_number(),
+                phone_number_2_type='cell',
+                how_learn='Social Media',
+                how_help='As an individual volunteer',
+                how_know='Friend',
+                skills_to_offer='No Selection',
+                password='password',
             )
 
             self.stdout.write(self.style.SUCCESS(f'Created user {i} of {num_users}'))
