0288a0b4bd5c888033ab15a9a25cb0ec1cde90cd
cx-lu <cxlu@wharton.upenn.edu>
2020-04-25 19:52:40 -0400

    quick fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -170,5 +170,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com' # mail service smtp
-EMAIL_HOST_USER = 'christina.lu.12@gmail.com' # email id
-EMAIL_HOST_PASSWORD = 'mygerbil812' #password
+EMAIL_HOST_USER = '' # email id
+EMAIL_HOST_PASSWORD = '' #password
