6a0eb9c9af032bc047445052cd9a4863479a32ad
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-06-02 00:06:59 -0400

    Changed email settings fields and use checkboxes

--- frontend/src/pages/EmailSettings.js
+++ frontend/src/pages/EmailSettings.js
@@ -1,79 +1,52 @@
-import React from "react"
+import React, { useCallback, useState } from "react"
 
 import Container from 'react-bulma-components/lib/components/container';
 import Columns from 'react-bulma-components/lib/components/columns'
 import Heading from 'react-bulma-components/lib/components/heading'
 
+import CheckboxField from '../components/checkboxfield'
 import SideNavAccount from '../components/sideNavAccount'
-import ToggleSwitch from '../components/switch'
-
-import { Link } from 'react-router-dom'
-import Button from 'react-bulma-components/lib/components/button'
-
 
 export default function AccountSettings() {
-    var containerStyle = {
-        margin: '5% auto',
-        maxWidth: '80%',
-        padding: '4rem',
-    }
+  var containerStyle = {
+    margin: '5% auto',
+    maxWidth: '80%',
+    padding: '4rem',
+  }
 
-    return (
-        <Container style={containerStyle}>
-            <Heading size={4} style={{margin:'0% 0% 3% 30%',}}>Email Settings</Heading>
-            <Columns>
-                <Columns.Column style={{ maxWidth:'30%' }}>
-                    <SideNavAccount />
-                </Columns.Column>
+  var checkboxStyle = {
+    fontSize: '0.75rem',
+    fontStyle: 'italic',
+    margin: '5px 0',
+    display: 'flex',
+    justifyContent: 'flex-start',
+  }
 
-                <Columns.Column>
-                <div>
-                   <ToggleSwitch/> <a style={{ marginLeft: '1.5rem' }}> </a>
-                   Information about Service Upgrades 
-                </div>
-                <br/>
-                <div>
-                   <ToggleSwitch/> <a style={{ marginLeft: '1.5rem' }}> </a>
-                   Lotsa Community Works Newsletter
-                </div>
-                 <br/>
-                <div>
-                   <ToggleSwitch/> <a style={{ marginLeft: '1.5rem' }}> </a>
-                   Information about Here To Serve
-                </div>
-                <br/>
-        
-                <Link to='/'>
-                <Button
-                  style={{
-                    marginBottom: '1rem',
-                    boxShadow: '1px 1px 3px 2px rgba(0,0,0,0.1)',
-                    maxWidth:250,
+  const [infoAboutH2S, setInfoAboutH2S] = useState(false)
+  const [newsletter, setNewsLetter]     = useState(false)
+  
+  const handleOnCheckInformationAboutH2S = useCallback(() => {
+    setInfoAboutH2S(!infoAboutH2S)
+  })
 
-                  }}
-                  color='primary'
-                  fullwidth={true}
-                >
-                  SAVE CHANGES
-                </Button>
-              </Link>
-                <Link to='/'>
-                  <Button
-                    style={{
-                      boxShadow: '1px 1px 3px 2px rgba(0,0,0,0.1)',
-                    maxWidth:250,
-                    }}
-                    fullwidth={true}
-                    outlined={true}
-                    color='primary'
-                  >
-                    CANCEL
-                  </Button>
-                </Link>
-                </Columns.Column>
-            </Columns>
+  const handleOnCheckNewsLetter = useCallback(() => {
+    setInfoAboutH2S(!newsletter)
+  })
 
-          
-        </Container>
-    );
+  return (
+    <Container style={containerStyle}>
+      <Heading size={4} style={{ margin: '0% 0% 3% 30%', }}>Email Settings</Heading>
+      <Columns>
+        <Columns.Column style={{ maxWidth: '30%' }}>
+          <SideNavAccount />
+        </Columns.Column>
+        <Columns.Column>
+        <CheckboxField text={'Information about family and Here to Serve'} onChange={handleOnCheckInformationAboutH2S} />
+        <CheckboxField text={'Receive Here to Serve newsletter'} onChange={handleOnCheckNewsLetter} />
+        </Columns.Column>
+      </Columns>
+
+
+    </Container>
+  );
 }
