644ae04f0898170590de36b524a8086b45637784
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-10-10 20:44:50 -0400

    Added build folder for frontend

---
+++ app/build/precache-manifest.6d07a53c4c30dc264c91f08dd1fa9784.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0db2757ef7644682af3b3c899c82bc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf3989eaaaa137b79bad",
+    "url": "/static/css/2.1e93ff57.chunk.css"
+  },
+  {
+    "revision": "761bae6369d291625e72",
+    "url": "/static/css/main.72a36c2a.chunk.css"
+  },
+  {
+    "revision": "bf3989eaaaa137b79bad",
+    "url": "/static/js/2.fa574c2e.chunk.js"
+  },
+  {
+    "revision": "c7273747c909dc4111a22bff6cf41781",
+    "url": "/static/js/2.fa574c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761bae6369d291625e72",
+    "url": "/static/js/main.45b96501.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "fce57c7627bde19582d3268562d64fff",
+    "url": "/static/media/families.fce57c76.png"
+  },
+  {
+    "revision": "3fc9e322a0af042d3d997c3bd23d468d",
+    "url": "/static/media/logo.3fc9e322.png"
+  },
+  {
+    "revision": "21c98c5f0e97c4e67d195581f4379939",
+    "url": "/static/media/waiting_approval.21c98c5f.png"
+  }
+]);
