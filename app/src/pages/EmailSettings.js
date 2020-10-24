2a86176b9112189559ede92702c3e3ca55cb7166
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-10-24 17:59:28 -0400

    removed axios

--- app/src/pages/EmailSettings.js
+++ app/src/pages/EmailSettings.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useCallback, useState } from "react"
-import axios from 'axios'
 
 import Container from 'react-bulma-components/lib/components/container';
 import Columns from 'react-bulma-components/lib/components/columns'
