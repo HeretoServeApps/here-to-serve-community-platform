2b01eeda6355bc277a72d50b6d491ddce73f8b79
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-10-31 16:16:06 -0400

    Set debug==false

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
